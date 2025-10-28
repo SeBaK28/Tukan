0434a75ed100414b24c322a17342f0aff8c629e4
SebastianK <sebast0382@gmail.com>
2025-10-28 18:10:31 +0100

    remove authorization bugs

--- api/Controllers/FamillyController.cs
+++ api/Controllers/FamillyController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using api.Data;
 using api.Models;
 using api.Models.DTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -16,20 +18,66 @@ namespace api.Controllers
     public class FamillyController : ControllerBase
     {
         private readonly UserManager<UserData> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public FamillyController(UserManager<UserData> userManager)
+        public FamillyController(UserManager<UserData> userManager, ApplicationDbContext context)
         {
+            _context = context;
             _userManager = userManager;
         }
 
-        [HttpGet]
-        [Route("createFamilly")]
+        [HttpPost]
+        [Route("create-familly")]
         [Authorize]
-        public async Task<IActionResult> CreateFamilly()
+        public async Task<IActionResult> CreateFamilly([FromBody] NewFamillyRequestDto requestDto)
         {
-            //var getId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var getId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var getUser = await _userManager.FindByIdAsync(getId);
 
-            return Ok("Success");
+            var familly = new FamillyData
+            {
+                NameGroup = requestDto.NameGroup,
+                GroupBudget = 0,
+                AdminGroupId = getUser.Id,
+                Members = new List<UserData>()
+            };
+
+            familly.Members.Add(getUser);
+            var addOperation = await _context.FamillyDatas.AddAsync(familly);
+            if (addOperation is not null)
+            {
+                await _userManager.AddToRoleAsync(getUser, "FamillyCreator");
+                await _context.SaveChangesAsync();
+
+                return Ok($"Familly {requestDto.NameGroup} is create");
+                // return CreatedAtAction(nameof(), new{id = })
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("add-member")]
+        [Authorize(Roles = "FamillyCreator")]
+        public async Task<IActionResult> AddUserToMemers([FromBody] AddUserToMemberDTO requestDto)
+        {
+            var creatorId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userToAdd = await _userManager.FindByEmailAsync(requestDto.Email);
+
+            var familly = await _context.FamillyDatas.FirstOrDefaultAsync(x => x.GroupId == requestDto.GroupId);
+
+            if(familly is null || userToAdd is null)
+            {
+                return NotFound();
+            }
+
+            if (familly.AdminGroupId == creatorId)
+            {
+                familly.Members.Add(userToAdd);
+                await _context.SaveChangesAsync();
+            }
+
+            return BadRequest();
         }
     }
 }
--- api/Program.cs
+++ api/Program.cs
@@ -46,21 +46,46 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequiredUniqueChars = 1;
 });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+//         options.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             AuthenticationType = "Jwt",
+//             ValidateIssuer = true,
+//             ValidateAudience = true,
+//             ValidateLifetime = true,
+//             ValidateIssuerSigningKey = true,
+//             ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//             ValidAudience = builder.Configuration["Jwt:Audience"],
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+//         };
+//     });
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme =
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            AuthenticationType = "Jwt",
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
-    });
+        AuthenticationType = "Jwt",
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 var app = builder.Build();
 
